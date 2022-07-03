aad0f53ec88ba19a1c8c148d769ab7601f81fdd5
sal9192 <76464649+sal9192@users.noreply.github.com>
2022-07-03 15:26:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom";
+import Checkout from'./Checkout';
+import Login from'./Login';
+function App() {
+  return (
+    //BEM 
+    <Router>
+    <div className="App">
+          
+        <Routes>
+  <Route path="/login" element={[<Login />]}></Route> 
+          <Route path="/checkout" element={[<Header />, <Checkout />]}/>
+          <Route path="/" element={[<Header />, <Home /> ]}/>
+         
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-G4ojG1bahtVUzFfRyIa2rtcCDABBrSY",
+    authDomain: "clone-d67e6.firebaseapp.com",
+    projectId: "clone-d67e6",
+    storageBucket: "clone-d67e6.appspot.com",
+    messagingSenderId: "44888357522",
+    appId: "1:44888357522:web:528ba76a827502660a044b",
+    measurementId: "G-RKHZ1S5137"
+  };
